638c57a5d2ed39f72867fe1fe7ceeae6a15ad1ee
Xinran Song <songxr0511@gmail.com>
2022-05-15 14:30:12 -0400

    finish LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,12 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Xinran Song
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
@@ -13,12 +14,18 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    public static boolean isLeapYear(int year) {
+        /**
+         * A leap year is either: divisible by 400 or divisible by 4 and not by 100.
+         */
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+            System.out.println("e.g. java LeapYear 2000");
         }
         for (int i = 0; i < args.length; i++) {
             try {
